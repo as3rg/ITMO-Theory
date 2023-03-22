1e083dbdd5b194f00a7f757081d733dde9c503c9
spyteamalex <spyteamalex@gmail.com>
2023-03-23 00:50:55 +0300

    Чт 23 мар 2023 00:50:55 MSK

---
+++ build.sh
@@ -0,0 +1,14 @@
+RED='\033[0;31m'          # Red
+GREEN='\033[0;32m'        # Green
+find ~+ -name "*.tex" | while read line
+do
+  cd "$(dirname "$line")"
+  pdflatex -interaction=nonstopmode "$line" 1> /dev/null
+  if [[ "$?" == "0" ]]; then
+    printf "$GREEN$line\n"
+  else
+    printf "$RED$line\n"
+  fi
+  # latexmk -pdf "$line" 1> /dev/null
+done
+find ~+ -name '*.aux' -o -name '*.fdb_latexmk' -o -name '*.log' -o -name '*.out' | tr "\n" "\0" | xargs -0 rm
